2b71e42ff467413f583a95f6e0f1af53a2f393df
Anas <anasansarimuhammad@gmail.com>
2022-08-16 17:18:34 +0500

    update code

--- cypress/e2e/testWithPageObjects.cy.js
+++ cypress/e2e/testWithPageObjects.cy.js
@@ -4,7 +4,18 @@ import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import { navigateTo } from "../support/page_objects/navigationPages"
 import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
+// To ignore and run On browsers functions
+const runOn = (browser, fn) => {
+     if(Cypress.isBrowser(browser)){
+          fn()
+     }
+} 
 
+const ignoreOn = (browser, fn) => {
+     if (!Cypress.isBrowser(browser)){
+          fn()
+     }
+}
 
 describe("Test with Page Objects",()=>{  // test suite declaration
     
@@ -12,18 +23,25 @@ describe("Test with Page Objects",()=>{  // test suite declaration
          cy.openHomePage() // called from commands.js
      })
 
+// to ignore this test on firefore browser
+ignoreOn('firefox', () => {
+// runOn('firefox', () => {
      it("Verifying navigations across the pages", ()=>{  //test declaration
-        //Page objects calling from NavigationPages predefined functions/methods 
-           navigateTo.formsLayoutsPage()  
-           navigateTo.datePickerPage()
-           navigateTo.toasterPage()
-           navigateTo.tooltipPPage()
-           navigateTo.smartTablePage()
+          //Page objects calling from NavigationPages predefined functions/methods 
+             navigateTo.formsLayoutsPage()  
+             navigateTo.datePickerPage()
+             navigateTo.toasterPage()
+             navigateTo.tooltipPPage()
+             navigateTo.smartTablePage()
+       
+       
+          })
 
+})  
+    
+     
 
-     })
-
-     it.only("Should submit the Inline form and Basic Form and select tomorrow date in the calender", () =>{
+     it("Should submit the Inline form and Basic Form and select tomorrow date in the calender", () =>{
           navigateTo.formsLayoutsPage()
           onFormLayoutsPage.submitInlineFormWithNameAndEmail('Anas007', 'aa@gmail.com') 
           onFormLayoutsPage.submitBasicFormWithEmailAndPassword('aa@gmail.com', 'password')
